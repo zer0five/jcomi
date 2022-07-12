1057f74525d74c192b78baefdc96735e51c264b2
K4zoku <kazoku0356@gmail.com>
2022-07-12 16:47:48 +0700

    updated DatabaseUtil

--- src/java/me/kazoku/jcomi/util/DatabaseUtil.java
+++ src/java/me/kazoku/jcomi/util/DatabaseUtil.java
@@ -1,6 +1,5 @@
 package me.kazoku.jcomi.util;
 
-import java.sql.Driver;
 import me.kazoku.core.database.sql.SQLDriver;
 import me.kazoku.core.database.sql.SQLSettings;
 import me.kazoku.core.database.sql.client.JavaSQLClient;
@@ -18,7 +17,7 @@ public class DatabaseUtil {
     }
 
     public static JavaSQLClient createClient() {
-        Properties config = new Properties();
+        Properties config = new Properties(System.getProperties());
         try {
             config.load(DatabaseUtil.class.getResourceAsStream("/config.properties"));
         } catch (Exception e) {
@@ -33,18 +32,23 @@ public class DatabaseUtil {
         SQLDriver driver = new MicrosoftSQLDriver(integratedSecurity, development);
         SQLSettings settings = driver.getDefaultSettings();
         Optional.ofNullable(config.getProperty("database.host"))
+                .filter(s -> !s.isEmpty())
                 .or(() -> Optional.ofNullable(System.getenv("DATABASE_HOST")))
                 .ifPresent(settings::setHost);
         Optional.ofNullable(config.getProperty("database.port"))
+                .filter(s -> !s.isEmpty())
                 .or(() -> Optional.ofNullable(System.getenv("DATABASE_PORT")))
                 .ifPresent(settings::setPort);
         Optional.ofNullable(config.getProperty("database.username"))
+                .filter(s -> !s.isEmpty())
                 .or(() -> Optional.ofNullable(System.getenv("DATABASE_USERNAME")))
                 .ifPresent(settings::setUsername);
         Optional.ofNullable(config.getProperty("database.password"))
+                .filter(s -> !s.isEmpty())
                 .or(() -> Optional.ofNullable(System.getenv("DATABASE_PASSWORD")))
                 .ifPresent(settings::setPassword);
         Optional.ofNullable(config.getProperty("database.name"))
+                .filter(s -> !s.isEmpty())
                 .or(() -> Optional.ofNullable(System.getenv("DATABASE_NAME")))
                 .ifPresent(settings::setDatabase);
         return new JavaSQLClient(settings, driver);
